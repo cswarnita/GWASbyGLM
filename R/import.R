7d6d31ba489621a935a7561fcadb456e01ae30b9
atitparajuli2020 <atit.parajuli@gmail.com>
2020-03-29 19:05:43 -0700

    updates
    removed example.R, manual.pdf and function import

--- R/import.R
+++ R/import.R
@@ -1,9 +1,9 @@
-#' Function to import input file
+#' Function to import data file
 #'
-#' @description This is the function to import data. The import data should have information about Phenotype (n by 1 form), Markers (Numeric as well as map file with n by m and m by 3 dimension respectively) and Covariates (n by t form) with n the number of individuals in row, m number of markers in column and t number of covariates in column
+#' @description A function to import data file. The import data contains information about Phenotype (n by 1 form), Markers (Numeric as well as map file with n by m and m by 3 dimension respectively) and Covariates (n by t form) with n the number of individuals in row, m number of markers in column and t number of covariates in column
 #'
-#' @param filename Name and Location of the file to be imported in txt format
-#'
+#' @param filename Input file to import (Requires Phenotype, genotype, map and covariates information)
+#' @format matrix
 #' @return data file
 #' @export
 #'
