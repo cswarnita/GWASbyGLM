030a5a339a7a1783238ac6a1435806386c20e209
atitparajuli2020 <atit.parajuli@gmail.com>
2020-03-29 23:14:07 -0700

    update
    changed files

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,12 @@
-## Code for preparaing the dataset for the package
-
+#' Code for preparaing the dataset for the package
+#
 library(usethis)   ##package for data generation for the package
 usethis::use_data("DATASET")
 
+data1<-load("/Users/Atit/Desktop/R_package/GWASbyGLM/cov.RData")
+data2<-load("/Users/Atit/Desktop/R_package/GWASbyGLM/Phenotype.RData")
+data3<-load("/Users/Atit/Desktop/R_package/GWASbyGLM/GD.RData")
+data4<-load("/Users/Atit/Desktop/R_package/GWASbyGLM/GM.RData")
 sample_cov<-data.frame(Covariates,row.names = 1)  ###sample  data for package
 sample_phenotype<-data.frame(Pheno,row.names = 1) ###sample data for package
 sample_map<-data.frame(GM, row.names = 1)          ###sample data for package
