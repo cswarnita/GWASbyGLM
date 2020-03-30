8946afdc4bdc1eb47c20d593a107df2ade1519c1
cswarnita <s.chakraborty4@wsu.edu>
2020-03-30 03:24:03 -0700

    Change in function : Included C=NULL

--- R/PCA.R
+++ R/PCA.R
@@ -16,15 +16,15 @@
 ##Remove PC's that are linearly dependent with the given Covariates
 ##User Specified: How many PC's to choose as co-factors
 
-PC=function(PC_no,X,C){
+PC=function(PC_no,X,C=NULL){
   PCA=prcomp(X)$x
   if(!is.null(C)){
     c=cor(PCA,C)
     tmp=rownames(c)[c[,2] >= 0.7 | c[,2]<= -0.7|c[,1] >= 0.7 | c[,1]<= -0.7]#PC's whose correlation with the given covariates are more than .7 or less than     -.7 are removed.
 
-    PCA=PCA[ , !(colnames(PCA) %in% tmp)]}
+    PCA=PCA[ , !(colnames(PCA) %in% tmp)]
+    rm(c)
+    rm(tmp)}
   PCA=PCA[,1:PC_no]
-  rm(c)
-  rm(tmp)
   return(PCA)
 }
